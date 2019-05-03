0cc23ca6363a78d7c6ef6799c49ec7abff2718ea
Christopher Van Der Westhuizen <cvander169@gmail.com>
2019-05-03 23:27:26 +0900

    Add Immutable annotation.
    This initial commit to the project adds the first annotation
    (Immutable). Subsequent changes will later introduce the related
    annotation processor and examples.

---
+++ com/github/cvanderw/annotation/Immutable.java
@@ -0,0 +1,17 @@
+package com.github.cvanderw.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/**
+ * Indicates that the annotated type is immutable.
+ *
+ * <p>If the annotated element is determined to not be immutable (given specification in Effective
+ * Java) then an error will be generated during compilation.
+ */
+@Documented
+@Target(ElementType.TYPE)
+// TODO: Consider including a retention type of SOURCE (this matches what is done for the Override
+// annotation).
+public @interface Immutable {}
